cf78e2725835225d5a800fd2cc44c02accf52dd1
Owen LE RAY <leray.owen@gmail.com>
2026-03-03 13:32:56 +0100

    s02

--- s02.py
+++ s02.py
@@ -31,28 +31,34 @@ LATTICE_W[0] = 4./9.
 LATTICE_W[1:5] = 1./9.
 LATTICE_W[5:] = 1./36.
 
-LATTICE_Cx = LATTICE_C[:,0]
-LATTICE_Cy = LATTICE_C[:,1]
+LATTICE_CX = LATTICE_C[:,0]
+LATTICE_CY = LATTICE_C[:,1]
 LATTICE_INVCS2 = 3.
+
 
 def equilibrium_distribution(N: np.array) -> np.array:
     rho,u,v = flow_properties(N)
     return equilibirum_from_moments(rho,u,v)
 
-def equilibirum_from_moments(rho: float, u: np.array, v: np.array) -> np.array:
-    vc = LATTICE_INVCS2 * (u*LATTICE_Cx + v*LATTICE_Cy)
-    Neq = rho*LATTICE_W*(1 + vc + (vc*vc)/2 - LATTICE_INVCS2*(u*u + v*v)/2)
+
+def equilibirum_from_moments(rho: float, u: np.array[float], v: np.array[float]) -> np.array[float]:
+    vc = LATTICE_INVCS2 * (u*LATTICE_CX 
+                           + v*LATTICE_CY)
+    Neq = rho*LATTICE_W*(1 
+                         + vc 
+                         + (vc*vc)/2 
+                         - LATTICE_INVCS2*(u*u + v*v)/2)
     return Neq
 
 
-def flow_properties(N: np.array) -> tuple[float, np.array, np.array]:
+def flow_properties(N: np.array) -> tuple[float, np.array[float], np.array[float]]:
     rho = np.sum(N)
-    u = np.sum(N * LATTICE_Cx) / rho
-    v = np.sum(N *LATTICE_Cy) / rho
+    u = np.sum(N * LATTICE_CX) / rho
+    v = np.sum(N * LATTICE_CY) / rho
     return rho, u, v
 
 rho, u, v = [1., 0.1, 0.]
-N =equilibirum_from_moments(rho, u, v)
+N = equilibirum_from_moments(rho, u, v)
 Neq = equilibrium_distribution(N)
 print(flow_properties(N))
 print(N)
